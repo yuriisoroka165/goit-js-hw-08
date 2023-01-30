d8315e8aeafc302e87174109aedd49e654805cd0
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-30 12:49:22 +0200

    task 1, 2 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function galleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            `;}).join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup(galleryItems));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframeOnPage = document.querySelector('iframe');
+const videoPlayer = new Player(iframeOnPage);
+
+//змінна яка бере в себе часову мітку з локального сховища (через функцію яка повертає цю мітку)
+let lastCurrentTime = getTimeStampFromLocalStorage();
+
+// перевірка на undefined
+if (lastCurrentTime === undefined) {
+    lastCurrentTime = 0;
+}
+
+// встановлюємо відео плеєру місце отаньої зупинки
+videoPlayer.setCurrentTime(lastCurrentTime);
+
+// відстежування події timeupdate та виклик функції отримання часової мітки, кожної секунди
+videoPlayer.on('timeupdate', throttle(() => {
+    getCurrentTimeStamp();
+    }, 1000)
+);
+
+// функція отримує часову мітку від відео плеєра та викликає функію запису в локальне сховище
+function getCurrentTimeStamp() {
+    videoPlayer.getCurrentTime().then(function (seconds) {
+        putTimeStampToLocalStorage(seconds);
+    })
+}
+
+// функція заносить часову мітку в локальне сховище з ключем videoplayer-current-time
+function putTimeStampToLocalStorage(currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+// функція отримує останню часову мітку з локального сховища
+function getTimeStampFromLocalStorage() {
+    return localStorage.getItem('videoplayer-current-time');
+}
