e347091049411b8fa30cada80a4b3cef61c73cbe
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-30 16:51:16 +0200

    changes in task 02

--- src/js/02-video copy.js
+++ src/js/02-video copy.js
@@ -1,13 +1,17 @@
 import Player from '@vimeo/player';
-import throttle from "lodash.throttle";
 
-const SAVED_TIME_STAMP = 'videoplayer-current-time';
+import throttle from "lodash.throttle";
 
 const iframeOnPage = document.querySelector('iframe');
 const videoPlayer = new Player(iframeOnPage);
 
 //змінна яка бере в себе часову мітку з локального сховища (через функцію яка повертає цю мітку)
-const lastCurrentTime = getTimeStampFromLocalStorage();
+let lastCurrentTime = getTimeStampFromLocalStorage();
+
+// перевірка на undefined
+if (lastCurrentTime === undefined) {
+    lastCurrentTime = 0;
+}
 
 // встановлюємо відео плеєру місце отаньої зупинки
 videoPlayer.setCurrentTime(lastCurrentTime);
@@ -24,16 +28,10 @@ function getCurrentTimeStamp() {
 
 // функція заносить часову мітку в локальне сховище з ключем videoplayer-current-time
 function putTimeStampToLocalStorage(currentTime) {
-    localStorage.setItem(SAVED_TIME_STAMP, currentTime);
+    localStorage.setItem('videoplayer-current-time', currentTime);
 }
 
 // функція отримує останню часову мітку з локального сховища
 function getTimeStampFromLocalStorage() {
-    const savedTimeStamp = localStorage.getItem(SAVED_TIME_STAMP);
-
-    if (savedTimeStamp) {
-        return savedTimeStamp;
-    } else {
-        return 0;
-    }
+    return localStorage.getItem('videoplayer-current-time');
 }
